4f14766d58b80485797120bd87514456086c9c92
EddyVinck <eddyvinck95@gmail.com>
2024-11-13 16:01:41 +0000

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   output: "static",
   integrations: [tailwind()],
+
   vite: {
     optimizeDeps: {
       exclude: ["shiki"],
     },
   },
+
+  adapter: netlify(),
 });
